e6ade4493d9e3abcd0b9d2ebc49500e3c2bb6231
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-06-11 13:22:45 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,51 @@
+//este es un comentario de linea
+/* 
+comentario
+de
+bloque
+*/
+
+
+/* //formas recomendadas
+let numero = '1234';
+const NUMERO = 1;
+
+//forma no recomendada
+var nombre = 'andres*';
+
+console.log(typeof(numero)); */
+
+/* let numero1 = 4;
+let numero2 = 5;
+let resultadoSuma = numero1 + numero2;
+let resultadoMultiplicacion = numero1 * numero2;
+let resultadoResta = numero1 - numero2;
+let resultadoDivision = numero1 / numero2;
+
+console.log(resultadoSuma);
+console.log(resultadoMultiplicacion);
+console.log(resultadoResta);
+console.log(resultadoDivision); */
+
+/* let texto1 = "Andres";
+let texto2 = "Marian";
+
+console.log(texto1 + " " + texto2); */
+
+/* let nombre = prompt("Ingrese el nombre de la persona");
+console.log(typeof(nombre)); */
+
+/* let numero1 = parseInt(prompt("Ingrese el numero 1"));
+let numero2 = parseInt(prompt("Ingrese el numero 2"));
+let resultadoSuma = numero1 + numero2;
+
+alert(numero1 + numero2); */
+/* let entrada = prompt("Ingresar una letra");
+let salida  = entrada + " " + "ingresada";
+alert(salida); */
+
+let nombre = "Andres";
+let apellido = "Ruiz";
+let edad = 70;
+
+console.log(`hola ${nombre} ${apellido} tu edad es ${edad}`);
