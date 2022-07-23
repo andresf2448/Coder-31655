a49ef27bdf1ab0090a2b69bf901b43ed430ba820
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-07-23 13:25:04 -0500

    Clase 8

--- Clase 8/bloque2/index.js
+++ Clase 8/bloque2/index.js
@@ -39,7 +39,7 @@ let boton = document.getElementById("btn");
   });
 }); */
 
-boton.addEventListener("click", () => {
+/* boton.addEventListener("click", () => {
   Swal.fire({
     ltite: "Está seguro de agregar el producto?",
     icon: "warning",
@@ -57,7 +57,105 @@ boton.addEventListener("click", () => {
         title: "Agregado!",
         icon: "success",
         text: "El archivo ha sido borrado",
+        background: "red"
       });
     }
   });
-});
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    style: {
+      background: "linear-gradient(to left, #00b09b, #96c92d)",
+    },
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    className: "amarillo"
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Probando toast!",
+    duration: 3000,
+    onClick: () => {
+      Toastify({
+        text: "Clickeaste un Toast!",
+        duration: 1500,
+      }).showToast();
+    },
+  }).showToast();
+}); */
+
+/* boton.addEventListener("click", () => {
+  Toastify({
+    text: "Click aquí para ir a Coder!",
+    duration: 3000,
+    destination: "https://www.coderhouse.com",
+  }).showToast();
+}); */
+
+const DateTime = luxon.DateTime;
+// const dt = DateTime.local(2022,7,23,12,53);
+// console.log(dt.c);
+// console.log(dt.toString());
+// console.log(dt.year);
+// console.log(dt.month);
+// console.log(dt.second);
+// console.log(dt.day);
+// console.log(dt.weekday);
+/* console.log(dt.zoneName);
+console.log(dt.daysInMonth); */
+// console.log(dt.toLocaleString());
+// console.log(dt.toLocaleString(DateTime.DATE_FULL));
+// console.log(dt.toLocaleString(DateTime.TIME_SIMPLE));
+// console.log(dt.setLocale("en").toLocaleString(DateTime.DATE_FULL));
+// console.log(dt.setLocale("es").toLocaleString(DateTime.DATE_FULL));
+
+const dt = DateTime.now();
+const Interval = luxon.Interval;
+
+// console.log( dt.toLocaleString(DateTime.DATETIME_SHORT) )
+
+// const suma = dt.plus({ hours: 5, minutes: 15 })
+// console.log(suma.toLocaleString(DateTime.DATETIME_SHORT))
+
+// const resta = dt.minus({ month: 2, days: 10 })
+// console.log(resta.toLocaleString(DateTime.DATETIME_SHORT))
+
+/* const now = DateTime.now();
+const later = DateTime.local(2022, 11, 25);
+const i = Interval.fromDateTimes(now, later);
+
+console.log(i.length('days'));
+console.log(i.length('hours'));
+console.log(i.length('minutes')); */
+
+Swal.fire({
+  title: 'Submit your Github username',
+  input: 'text',
+  inputAttributes: {
+    autocapitalize: 'off'
+  },
+  showCancelButton: true,
+  confirmButtonText: 'Look up',
+  allowOutsideClick: () => !Swal.isLoading()
+}).then((result) => {
+  if (result.isConfirmed) {
+    console.log(result.value);
+  }
+})
